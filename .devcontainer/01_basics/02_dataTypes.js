d2e180c43fb5ba7e46ad43f6404bd1113b3aec30
hex0143 <90190629+hex0143@users.noreply.github.com>
2023-09-11 13:43:28 +0000

    04_commit

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -1,12 +1,36 @@
 "use strict"; // treat all JS code as newer version 
+/*
 
-// alert(3 + 3) // we are useing nodejs , not browser
+JavaScript is a dynamically typed language, which means that variables in JavaScript can hold values of different data types. JavaScript has several data types, which can be broadly categorized into two categories: primitive data types and reference data types.
 
-// type of data type 
-// 1. premitive 2.non
-// number => 2 to power 53
-// bigint 
-// string = true/false
-// null => standalone value
-// undefined =>
-// symbol => unique
+Primitive Data Types:
+
+Number: Represents both integer and floating-point numbers. For example: let num = 42;
+String: Represents a sequence of characters enclosed in single or double quotes. For example: let str = "Hello, World!";
+Boolean: Represents a true or false value. For example: let isTrue = true;
+Undefined: Represents a variable that has been declared but has not been assigned a value. For example: let x;
+Null: Represents an intentional absence of any object value. For example: let y = null;
+Symbol (ES6): Represents a unique and immutable value used as object property keys. For example: const sym = Symbol("description");
+BigInt (ES11): Represents large integers that cannot be represented by the Number data type. For example: const bigNum = 1234567890123456789012345678901234567890n;
+Reference Data Types:
+
+Object: Represents a collection of key-value pairs and is used to store complex data structures. For example:
+javascript
+Copy code
+let person = {
+  name: "John",
+  age: 30
+};
+Array: A special type of object that holds an ordered collection of values. For example: let numbers = [1, 2, 3, 4, 5];
+Function: A type that represents executable code. Functions are also objects in JavaScript. For example:
+javascript
+Copy code
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+JavaScript is a dynamically typed language, which means that you do not need to specify the data type of a variable when declaring it; the interpreter determines the data type at runtime based on the assigned value. This flexibility makes JavaScript a versatile language, but it also requires careful handling of data types to prevent unexpected behavior.
+
+*/
+
+  
+
