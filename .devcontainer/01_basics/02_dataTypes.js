50b051069695d221be7a36ff90c36439292772dd
hex0143 <90190629+hex0143@users.noreply.github.com>
2023-09-11 13:42:50 +0000

    03_commit

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) // we are useing nodejs , not browser
+
+// type of data type 
+// 1. premitive 2.non
+// number => 2 to power 53
+// bigint 
+// string = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
